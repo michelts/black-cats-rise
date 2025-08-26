e478d0dd5fc5c9c06606f0f56ca6d564ab225551
Michel Sabchuk <michel@turbosys.com.br>
2025-08-26 01:05:58 -0300

    Add test proving that there are repeated matches

--- src/core/__tests__/matches.test.ts
+++ src/core/__tests__/matches.test.ts
@@ -9,20 +9,9 @@ it("generates matches", () => {
   expect(matches).toHaveLength(15);
   expect(getCount(matches, teamsCount, "home")).toEqual([3, 2, 2, 2, 3, 3]);
   expect(getCount(matches, teamsCount, "away")).toEqual([2, 3, 3, 3, 2, 2]);
-  for (let i = 0; i < teamsCount - 1; i++) {
-    const roundMatches = matches.filter((match) => match.round === i);
-    expect(roundMatches).toHaveLength(3);
-    const teams = new Set(
-      roundMatches.flatMap((match) => [match.home.teamId, match.away.teamId]),
-    );
-    expect(teams).toHaveLength(6); // make sure there's not repeating inside the round
-  }
-  for (let i = 0; i < teamsCount; i++) {
-    const teamRounds = matches
-      .filter((match) => match.home.teamId === i || match.away.teamId === i)
-      .map((match) => match.round);
-    expect(teamRounds).toEqual(range(5)); // ensure index are sorted
-  }
+  assertRoundHasAllTeams(matches, teamsCount);
+  assertRoundsAreSortedAscending(matches, teamsCount);
+  assertUniqueMatches(matches);
 });
 
 it("generates matches with 20 teams", () => {
@@ -35,20 +24,9 @@ it("generates matches with 20 teams", () => {
   expect(getCount(matches, teamsCount, "away")).toEqual([
     9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 9, 9, 9, 9, 9,
   ]);
-  for (let i = 0; i < teamsCount - 1; i++) {
-    const roundMatches = matches.filter((match) => match.round === i);
-    expect(roundMatches).toHaveLength(10);
-    const teams = new Set(
-      roundMatches.flatMap((match) => [match.home.teamId, match.away.teamId]),
-    );
-    expect(teams).toHaveLength(20); // make sure there's not repeating inside the round
-  }
-  for (let i = 0; i < teamsCount; i++) {
-    const teamRounds = matches
-      .filter((match) => match.home.teamId === i || match.away.teamId === i)
-      .map((match) => match.round);
-    expect(teamRounds).toEqual(range(19)); // ensure index are sorted
-  }
+  assertRoundHasAllTeams(matches, teamsCount);
+  assertRoundsAreSortedAscending(matches, teamsCount);
+  assertUniqueMatches(matches);
 });
 
 function getCount(matches: StoredMatch[], count: number, key: "home" | "away") {
@@ -56,4 +34,41 @@ function getCount(matches: StoredMatch[], count: number, key: "home" | "away") {
   return indexes.map(
     (index) => matches.filter((match) => match[key].teamId === index).length,
   );
+}
+
+function assertRoundHasAllTeams(matches: StoredMatch[], teamsCount: number) {
+  for (let i = 0; i < teamsCount - 1; i++) {
+    const roundMatches = matches.filter((match) => match.round === i);
+    expect(roundMatches).toHaveLength(teamsCount / 2);
+    const teams = new Set(
+      roundMatches.flatMap((match) => [match.home.teamId, match.away.teamId]),
+    );
+    expect(teams).toHaveLength(teamsCount); // make sure there's not repeating inside the round
+  }
+}
+
+function assertRoundsAreSortedAscending(
+  matches: StoredMatch[],
+  teamsCount: number,
+) {
+  for (let i = 0; i < teamsCount; i++) {
+    const teamRounds = matches
+      .filter((match) => match.home.teamId === i || match.away.teamId === i)
+      .map((match) => match.round);
+    expect(teamRounds).toEqual(range(teamsCount - 1)); // ensure index are sorted
+  }
+}
+
+function assertUniqueMatches(matches: StoredMatch[]) {
+  const result: string[] = [];
+  for (const match of matches) {
+    const ids = [match.home.teamId, match.away.teamId];
+    ids.sort();
+    const key = `${ids[0]}-${ids[1]}`;
+    if (result.includes(key)) {
+      console.log("Duplicate key", key);
+      throw new Error(`Duplicate: ${key}`);
+    }
+    result.push(key);
+  }
 }
